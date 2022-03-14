1ef2a27066b7ab3258401c41337632b9e20cc9ae
Aaron Smith <32134610+aaroncolesmith@users.noreply.github.com>
2022-03-14 16:06:45 -0700

    revert

--- app.py
+++ app.py
@@ -1,79 +1,79 @@
 import streamlit as st
 
-PAGES = {
-"About": 'about',
-"Experience": 'experience',
-"Projects": 'projects',
-"Bovada": 'bovada'
-}
+# PAGES = {
+# "About": 'about',
+# "Experience": 'experience',
+# "Projects": 'projects',
+# "Bovada": 'bovada'
+# }
 
 
-try:
-    options = list(PAGES.keys())
-    query_params=st.experimental_set_query_params()
-    query_option = query_params['option'][0]
-
-    st.sidebar.title('Navigation')
-    option_selected = st.sidebar.selectbox('Pick option',
-                                            options,
-                                            index=options.index(query_option))
-
-    
-    # sel = st.sidebar.radio("Go to", list(PAGES.keys()))
-    if option_selected:
-        page = PAGES[option_selected]
-
-        st.write(page)
-        st.experimental_set_query_params(option=option_selected)
-
-except:
-
-    options = list(PAGES.keys())
-    st.write(options)
-    st.experimental_set_query_params(option=options[1])
-
-
-    query_params = st.experimental_get_query_params()
-    query_option = query_params['option'][0]
-
-    st.sidebar.title('Navigation')
-    option_selected = st.sidebar.selectbox('Pick option',
-                                            options,
-                                            index=options.index(query_option))
-
-    
-    # sel = st.sidebar.radio("Go to", list(PAGES.keys()))
-    if option_selected:
-        page = PAGES[option_selected]
-
-        st.write(page)
-        st.experimental_set_query_params(option=option_selected)
-
-
-
-# # query params exist
 # try:
-#     options = ['cat', 'dog', 'mouse', 'bat', 'duck']
+#     options = list(PAGES.keys())
+#     query_params=st.experimental_set_query_params()
+#     query_option = query_params['option'][0]
 
-#     query_params = st.experimental_get_query_params()
-#     query_option = query_params['option'][0] #throws an exception when visiting http://host:port
-
+#     st.sidebar.title('Navigation')
 #     option_selected = st.sidebar.selectbox('Pick option',
 #                                             options,
 #                                             index=options.index(query_option))
+
+    
+#     # sel = st.sidebar.radio("Go to", list(PAGES.keys()))
 #     if option_selected:
+#         page = PAGES[option_selected]
+
+#         st.write(page)
 #         st.experimental_set_query_params(option=option_selected)
 
-# # run when query params don't exist. e.g on first launch
-# except: # catch exception and set query param to predefined value
-#     options = ['cat', 'dog', 'mouse', 'bat', 'duck']
-#     st.experimental_set_query_params(option=options[1]) # defaults to dog
+# except:
+
+#     options = list(PAGES.keys())
+#     st.write(options)
+#     st.experimental_set_query_params(option=options[1])
+
 
 #     query_params = st.experimental_get_query_params()
 #     query_option = query_params['option'][0]
 
+#     st.sidebar.title('Navigation')
 #     option_selected = st.sidebar.selectbox('Pick option',
 #                                             options,
 #                                             index=options.index(query_option))
+
+    
+#     # sel = st.sidebar.radio("Go to", list(PAGES.keys()))
 #     if option_selected:
+#         page = PAGES[option_selected]
+
+#         st.write(page)
 #         st.experimental_set_query_params(option=option_selected)
+
+
+
+# query params exist
+try:
+    options = ['cat', 'dog', 'mouse', 'bat', 'duck']
+
+    query_params = st.experimental_get_query_params()
+    query_option = query_params['option'][0] #throws an exception when visiting http://host:port
+
+    option_selected = st.sidebar.selectbox('Pick option',
+                                            options,
+                                            index=options.index(query_option))
+    if option_selected:
+        st.experimental_set_query_params(option=option_selected)
+
+# run when query params don't exist. e.g on first launch
+except: # catch exception and set query param to predefined value
+    options = ['cat', 'dog', 'mouse', 'bat', 'duck']
+    st.experimental_set_query_params(option=options[1]) # defaults to dog
+
+    query_params = st.experimental_get_query_params()
+    query_option = query_params['option'][0]
+
+    option_selected = st.sidebar.selectbox('Pick option',
+                                            options,
+                                            index=options.index(query_option))
+    if option_selected:
+        st.experimental_set_query_params(option=option_selected)
