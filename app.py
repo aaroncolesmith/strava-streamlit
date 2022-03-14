2faa3cd63054be105b4a792227c277f0f140d77f
Aaron Smith <32134610+aaroncolesmith@users.noreply.github.com>
2022-03-14 16:34:32 -0700

    f

--- app.py
+++ app.py
@@ -114,14 +114,22 @@ PAGES = {
 #     st.write(option_selected)
 
 
-options = list(PAGES.keys())
+pages = list(PAGES.keys())
 query_params = st.experimental_get_query_params()
 try:
     query_option = query_params['page'][0]
 except:
-    st.experimental_set_query_params(page=options[1])
+    st.experimental_set_query_params(page=pages[0])
     query_params = st.experimental_get_query_params()
 
-
 st.write(query_params)
+
+
+st.sidebar.title('Navigation')
+page_selected = st.sidebar.selectbox('Pick option',
+                                        pages,
+                                        index=pages.index(query_option))
+
+st.write(page_selected)
+st.write(PAGES[page_selected])
 
