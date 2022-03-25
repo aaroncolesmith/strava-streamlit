ebbc545a43322a83a5425fc510fa709cbb58b75e
aaron <aaroncolesmith@gmail.com>
2022-03-25 15:00:10 -0700

    d

--- app.py
+++ app.py
@@ -8,9 +8,8 @@ def main():
     df = pd.read_csv('https://raw.githubusercontent.com/aaroncolesmith/portland_crime_map/main/data.csv')
     st.title('Portland Crime Map')
     st.markdown('This app is a Streamlit dashboard that shows the number of crimes in Portland, Oregon.')
-    st.markdown('**Data Source:** [https://data.pdx.gov/api/views/v8jb-q3jb/rows.csv](https://data.pdx.gov/api/views/v8jb-q3jb/rows.csv)')
-    st.markdown('**Data Description:** [https://data.pdx.gov/api/views/v8jb-q3jb/](https://data.pdx.gov/api/views/v8jb-q3jb/)')
-    st.markdown('**Data License:** [https://data.pdx.gov/api/views/v8jb-q3jb/](https://data.pdx.gov/api/views/v8jb-q3jb/)')
+
+    st.markdown('Updated as of: ' + df['Date'].max())
 
     st.subheader('Crime Counts by Type')
     st.write(df.groupby('CRIME').size().sort_values(ascending=False).head(10))
