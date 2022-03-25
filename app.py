e4e909f5283bb1e467f6c5f0e11466f3f88e6106
aaron <aaroncolesmith@gmail.com>
2022-03-25 15:14:24 -0700

    a

--- app.py
+++ app.py
@@ -8,9 +8,12 @@ def main():
     df = pd.read_csv('https://raw.githubusercontent.com/aaroncolesmith/portland_crime_map/main/data.csv')
     df['DATE'] = pd.to_datetime(df['DATE'],errors='coerce')
     # df['DATE'] = df['DATE'].dt.tz_localize('UTC').dt.tz_convert('US/Pacific')
-
-
+    
+    
+    st.write(df.head(5))
     st.write(df.loc[df['DATE'].isnull()])
+
+    st.write(df.dtypes)
 
     st.title('Portland Crime Map')
     st.markdown('This app is a Streamlit dashboard that shows the number of crimes in Portland, Oregon.')
