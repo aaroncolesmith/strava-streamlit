b87bf6cc713a69c8b6ee6f9b0470ace69de635bc
aaron <aaroncolesmith@gmail.com>
2022-03-25 15:10:49 -0700

    a

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 def main():
     df = pd.read_csv('https://raw.githubusercontent.com/aaroncolesmith/portland_crime_map/main/data.csv')
-    df['DATE'] = pd.to_datetime(df['DATE'])
+    df['DATE'] = pd.to_datetime(df['DATE'],errors='coerce')
     df['DATE'] = df['DATE'].dt.tz_localize('UTC').dt.tz_convert('US/Pacific')
 
     st.title('Portland Crime Map')
