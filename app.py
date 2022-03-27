5200a72ef7beb125b2e5422c57779066ca6e580c
aaron <aaroncolesmith@gmail.com>
2022-03-26 19:17:20 -0700

    a

--- app.py
+++ app.py
@@ -14,11 +14,12 @@ def main():
     df['DATE'] = df['DATE'].dt.tz_convert('US/Pacific')
     df['HOUR'] = df['DATE'].dt.floor('h')
     df['DAY'] = df['DATE'].dt.floor('d')
+    df['DATE_CRIME'] = df['DATE'].dt.strftime('%-m-%-d %-I:%M%p').astype('str') + ' - ' + df['CRIME']
 
     st.title('Portland Crime Map')
     st.markdown('This app is a Streamlit dashboard that shows the number of crimes in Portland, Oregon.')
 
-    st.markdown('Updated as of: ' + str(df['DATE'].max().strftime('%Y-%m-%d %H:%M')))
+    st.markdown('Updated as of: ' + str(df['DATE'].max().strftime('%-m-%-d %-I:%M%p')))
 
     st.subheader('Crime Counts by Type')
     st.write(df.groupby('CRIME').size().sort_values(ascending=False).head(10))
@@ -34,28 +35,19 @@ def main():
     fig = px.bar(df.groupby(['HOUR','CRIME']).size().to_frame('COUNT').reset_index(), x='HOUR', y='COUNT', color='CRIME')
     st.plotly_chart(fig)
 
-    # start_time = st.slider('Start Date', 
-    #     df['DATE'].min().to_pydatetime(),
-    #     format="MM/DD/YY - hh:mm")
-
-    # d=df.loc[df.DATE >= start_time].groupby(['LATITUDE','LONGITUDE']).agg({'CRIME': lambda x: ', '.join(x),
-    #                                        'ID': 'size'}).reset_index()
-
-    d=df.groupby(['LATITUDE','LONGITUDE','ADDRESS']).agg({'CRIME': lambda x: ', '.join(x),
+    d=df.groupby(['LATITUDE','LONGITUDE','ADDRESS']).agg({'DATE_CRIME': lambda x: ', '.join(x),
                                             'ID': 'size',
                                             'DATE':'max'}).reset_index()
 
     d.columns = ['LATITUDE','LONGITUDE','ADDRESS','CRIME','COUNT','LAST_DATE']
     d['LATITUDE'] = pd.to_numeric(d['LATITUDE'])
     d['LONGITUDE'] = pd.to_numeric(d['LONGITUDE'])
-    d['CRIME'] = d['CRIME'].str.wrap(50)
-    d['CRIME'] = d['CRIME'].apply(lambda x: x.replace('\n', '<br>'))
     d['COUNT_SCALED'] = d['COUNT']*3
 
     d['LAT_LON'] = d['LATITUDE'].astype('str') + ', ' +  d['LONGITUDE'].astype('str')
 
     # if the crime desciption is greater than 500 characters, cut it off at 500 characters
-    d['CRIME'] = d['CRIME'].apply(lambda x: x[:500] + '...' if len(x) > 500 else x)
+    d['CRIME'] = d['CRIME'].apply(lambda x: x[:1500] + '...' if len(x) > 1500 else x)
 
     st.subheader('Crime Map')
     fig = px.density_mapbox(d, 
@@ -86,18 +78,16 @@ def main():
     fig.update_layout(mapbox_style="open-street-map")
     st.plotly_chart(fig)
 
-    d=df.groupby(['LATITUDE','LONGITUDE','ADDRESS','DAY']).agg({'CRIME': lambda x: ', '.join(x),
+    d=df.groupby(['LATITUDE','LONGITUDE','ADDRESS','DAY']).agg({'DATE_CRIME': lambda x: ', '.join(x),
                                             'ID': 'size',
                                             'DATE':'max'}).reset_index()
     d.columns = ['LATITUDE','LONGITUDE','ADDRESS','DAY','CRIME','COUNT','LAST_DATE']
     d['LATITUDE'] = pd.to_numeric(d['LATITUDE'])
     d['LONGITUDE'] = pd.to_numeric(d['LONGITUDE'])
-    d['CRIME'] = d['CRIME'].str.wrap(50)
-    d['CRIME'] = d['CRIME'].apply(lambda x: x.replace('\n', '<br>'))
     d['LAT_LON'] = d['LATITUDE'].astype('str') + ', ' +  d['LONGITUDE'].astype('str')
 
     # if the crime desciption is greater than 500 characters, cut it off at 500 characters
-    d['CRIME'] = d['CRIME'].apply(lambda x: x[:500] + '...' if len(x) > 500 else x)
+    d['CRIME'] = d['CRIME'].apply(lambda x: x[:1500] + '...' if len(x) > 1500 else x)
     d['DAY']=d['DAY'].dt.tz_localize(None).dt.to_pydatetime()
     d['COUNT_SCALED'] = d['COUNT']*5
 
